ef417a0c21376e9778e4a58f9ec6159220b9bae8
crystal-zq-wang <55931415+crystal-zq-wang@users.noreply.github.com>
2019-10-26 18:49:27 -0700

    Add files via upload

---
+++ skinnyvideos.py
@@ -0,0 +1,104 @@
+from google.cloud import translate_v2, speech_v1, texttospeech
+import io
+
+
+class Video_Translator:
+
+    def __init__(self):
+
+        #BEGIN TRANSLATION SETUP
+        self.translate_client = translate_v2.Client()
+        #END TRANSLATION SETUP
+
+        #BEGIN GENERAL SETUP
+        self.languages = {}
+        for lng in self.translate_client.get_languages():
+            self.languages[lng['name'].lower()] = lng['language']
+        #END GENERAL SETUP
+
+        #BEGIN AUDIO-TEXT SETUP
+        self.audio_text_client = speech_v1.SpeechClient()
+        self.audio_channel_count = 2
+        self.enable_separate_recognition_per_channel = True
+        #END AUDIO-TEXT SETUP
+
+        #START TEXT-AUDIO SETUP
+        self.text_audio_client = texttospeech.TextToSpeechClient()
+        
+        #END TEXT-AUDIO SETUP
+
+    def translate(self, text, lng="english"):
+        translation = self.translate_client.translate(text,
+            target_language=self.languages[lng.lower()])
+        return self.edit_transcript(translation['translatedText'])
+        
+    def get_audio(self, local_file_path):
+        with io.open(local_file_path, "rb") as f:
+            content = f.read()
+        return content
+
+    def translate_video(self, url, native_lng, lng="english"):
+        #video, audio = self.retrieve_video_and_audio(url)
+        audio = {"content": self.get_audio(url)}
+        
+        #full_transcript = 
+        full_transcript = self.get_transcript(audio, native_lng).split(".")
+        translated_transcript = []
+        for line in full_transcript:
+            translated_transcript.append(self.translate(line, lng))
+        #print(translated_transcript)
+        return translated_transcript
+
+
+    def edit_transcript(self, transcript):
+        return transcript.replace("&#39;", "'")
+        #translated_audio
+        
+        #translated_audio = None #REPLACE THIS
+        #for i in range(len(full_transcript)):
+        #    native_line = full_transcript(i)
+        #    translated_line = translated_transcript(i)
+        #    speed_factor = self.get_speed_factor(native_line, translated_line)
+        #    translated_audio += self.text_to_audio(translated_line, speed_factor)
+
+        #return self.splice_video_and_audio(video, translated_audio)
+
+    def retrieve_video_and_audio(self, url): #ARUSHI HAS THIS CODE
+        return None
+
+    def get_transcript(self, audio, native_lng): #CRYSTAL HAS THIS CODE
+        config = {
+            "audio_channel_count": self.audio_channel_count,
+            "enable_separate_recognition_per_channel": self.enable_separate_recognition_per_channel,
+            "language_code": self.languages[native_lng],
+        }
+        response = self.audio_text_client.recognize(config, audio)
+        for result in response.results:
+            alternative = result.alternatives[0]
+
+        return format(alternative.transcript)
+
+    def get_speed_factor(native_line, translated_line): #CAN EDIT THIS LATER, FUNCTIONAL FOR NOW
+        return len(translated_line)/len(native_line)
+
+    def text_to_audio(self, text, lng, speed_factor=1, gender=None): #CRYSTAL IS WORKING ON THIS CODE
+        
+        if gender == "female":
+            ssml_gender=texttospeech.enums.SsmlVoiceGender.FEMALE
+        elif gender == "male":
+            ssml_gender=texttospeech.enums.SsmlVoiceGender.MALE
+        else:
+            ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL
+
+        synthesis_input = texttospeech.types.SynthesisInput(text=text)
+        voice = texttospeech.types.VoiceSelectionParams(language_code=self.languages[lng], ssml_gender=ssml_gender)
+        audio_config = texttospeech.types.AudioConfig(audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+        response = self.text_audio_client.synthesize_speech(synthesis_input, voice, audio_config)
+        with open('output.mp3', 'wb') as out:
+            # Write the response to the output file.
+            out.write(response.audio_content)
+            print('Audio content written to file "output.mp3"')
+            
+
+    def splice_video_and_audio(self, video, audio): # I SUPPOSE I(ANTON) WILL WORK ON THIS NOW
+        return None
