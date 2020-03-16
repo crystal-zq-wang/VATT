8a6be06d006581d00ea5a3cb64cf03347064e585
Crystal Wang <crystalwang@berkeley.edu>
2020-03-15 22:40:51 -0700

    added long video format

--- longvideo.py
+++ longvideo.py
@@ -11,8 +11,6 @@ from time import sleep
 from google.cloud import translate_v2, speech_v1, texttospeech
 from moviepy.editor import *
 
-
-#This function centers the text vertically in the terminal
 def enters(n):
     row= os.get_terminal_size().lines
     enter= (row-n)/2
@@ -31,7 +29,6 @@ def prints(str, n, input_size=0):
     for i in str:
         sleep(n)
         print(i, end='', flush=True)
-
 
 class VideoTranslator:
 
@@ -65,15 +62,15 @@ class VideoTranslator:
     def get_text(self, transcript):
         return transcript[0] 
         
-    def get_audio(self, local_file_path):
-        with io.open(local_file_path, "rb") as f:
-            content = f.read()
-        return content
+    # def get_audio(self, local_file_path):
+    #     with io.open(local_file_path, "rb") as f:
+    #         content = f.read()
+    #     return content
 
 
     def translate_video(self, url, native_lng, lng="english"):
         #video, audio = self.retrieve_video_and_audio(url)
-        audio = {"content": self.get_audio(url)}
+        audio = {"uri": url}
         
         full_transcript = self.split_transcript(self.get_transcript(audio, native_lng))
 
@@ -140,10 +137,12 @@ class VideoTranslator:
         with open("transcriptfinal.txt", "w") as out:
             out.write(display_text)
 
-        with open("output.mp3", "wb") as out:
-            out.write(translated_audio2)
+        # with open("output.mp3", "wb") as out:
+        #     out.write(translated_audio2)
 
-        audio_background = AudioFileClip("output.mp3")
+        stop = input("edit audio")
+
+        audio_background = AudioFileClip("chineseorigamibgm.mp3")
 
         final_audio = CompositeAudioClip([audio_background])
         final_clip = videoclip.set_audio(audio_background)
@@ -171,56 +170,79 @@ class VideoTranslator:
             "enable_separate_recognition_per_channel": self.enable_separate_recognition_per_channel,
             "language_code": self.languages[native_lng],
         }
-        response = self.audio_text_client.recognize(config, audio)
-        for result in response.results:
-            alternative = result.alternatives[0]
 
-        return format(alternative.transcript)
+        operation = self.audio_text_client.long_running_recognize(config, audio)
+        response = operation.result()
+
+        hi = input("stop")
+
+        for result in response.results:
+            # First alternative is the most probable result
+            alternative = result.alternatives[0]
+            print(alternative)
+            #print(u"Transcript: {}".format(alternative.transcript))
+
+        return format(alternative.transcript)    
+        # alternative = response.alternatives[0]
+        # # for result in response.results:
+        # #     alternative = result.alternatives[0]
+
+        # return format(alternative.transcript)
+
+        # encoding = enums.RecognitionConfig.AudioEncoding.LINEAR16
+        # config = {
+        #     "sample_rate_hertz": sample_rate_hertz,
+        #     "language_code": language_code,
+        #     "encoding": encoding,
+        # }
+        # audio = {"uri": storage_uri}
+
+        # operation = client.long_running_recognize(config, audio)
+
+        # print(u"Waiting for operation to complete...")
+        # response = operation.result()
+
+        # for result in response.results:
+        #     # First alternative is the most probable result
+        #     alternative = result.alternatives[0]
+        #     print(u"Transcript: {}".format(alternative.transcript))
+
 
     def get_speed_factor(self, native_line, translated_line): # incomplete
         return len(translated_line)/len(native_line)
 
-    def determine_gender(self, frequency):
-        return frequency > 170 and "female" or "male"
+    # def determine_gender(self, frequency):
+    #     return frequency > 170 and "female" or "male"
 
     def text_to_audio(self, text, lng, speed_factor, gender=None): 
         
-        gender = self.determine_gender(frequency)
+        # gender = self.determine_gender(frequency)
 
-        if gender == "female":
-            ssml_gender=texttospeech.enums.SsmlVoiceGender.FEMALE
-        elif gender == "male":
-            ssml_gender=texttospeech.enums.SsmlVoiceGender.MALE
-        else:
-            ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL
+        # if gender == "female":
+        #     ssml_gender=texttospeech.enums.SsmlVoiceGender.FEMALE
+        # elif gender == "male":
+        #     ssml_gender=texttospeech.enums.SsmlVoiceGender.MALE
+        # else:
+        #     ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL
 
         # print(speed_factor)
         synthesis_input = texttospeech.types.SynthesisInput(text=text)
         voice = texttospeech.types.VoiceSelectionParams(language_code=self.languages[lng], ssml_gender=texttospeech.enums.SsmlVoiceGender.MALE) #MALE DEFAULT FOR NOW
-        audio_config = texttospeech.types.AudioConfig(audio_encoding=texttospeech.enums.AudioEncoding.MP3, speaking_rate=speed_factor, pitch=frequency/100)
+        audio_config = texttospeech.types.AudioConfig(audio_encoding=texttospeech.enums.AudioEncoding.MP3, speaking_rate=speed_factor) #leaving out pitch
         response = self.text_audio_client.synthesize_speech(synthesis_input, voice, audio_config)
         return response.audio_content
 
 
-os.system("clear")
-
-# Takes in the name of the input file, which is a .mov file
 enters(1)
 prints("Specify Filename: ", 0.01, 10)
 s= input()
 
-# Getting the dimensions of the original video
 vid = cv2.VideoCapture(s)
 h = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
 w = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
 
-# Making an audiofile out of the the video file, named "trying.wav"
 videoclip = VideoFileClip(s)
-audioclip = videoclip.audio
-audioclip.write_audiofile("trying.wav", verbose=False)
-os.system("clear")
 
-# taking inputs of native language and changin language
 enters(1)
 prints("Input Language: ", 0.01, 8)
 s1 = str(input()).strip()
@@ -231,40 +253,106 @@ print()
 prints("Output Language: ", 0.01, 8)
 s2 = str(input()).strip()
 
-#finsing average frequency of the input audio
-lst = list()
-os.system("clear")
-os.system("crepe trying.wav --step-size 100")
+# lst = list()
+# os.system("clear")
+# os.system("crepe trying.wav --step-size 100")
 
-with open ('trying.f0.csv',newline='') as csvfile:
-    data = csv.reader(csvfile, delimiter=',')
-    for row in data:
-        lst.append(row[1])
+# with open ('trying.f0.csv',newline='') as csvfile:
+#     data = csv.reader(csvfile, delimiter=',')
+#     for row in data:
+#         lst.append(row[1])
 
-lst = lst[1:]
-lst = [float(s) for s in lst]
-lst.sort()
-length = len(lst)
+# lst = lst[1:]
+# lst = [float(s) for s in lst]
+# lst.sort()
+# length = len(lst)
 
-DELTA = 0.1
-lst = lst[int(length*DELTA):-int(length*DELTA)]
-frequency = sum(lst) / len(lst)
+# DELTA = 0.1
+# lst = lst[int(length*DELTA):-int(length*DELTA)]
+# frequency = sum(lst) / len(lst)
 
-os.system("clear")
+# os.system("clear")
 
-""" Ideas: Randomized language """
 
 vt = VideoTranslator()
 os.system("clear")
-vt.translate_video("trying.wav", s1, s2)
+vt.translate_video("gs://vatt/origami.wav", s1, s2) # input with your own google cloud bucket
 os.system("clear")
-os.remove("trying.wav")
+#os.remove("trying.wav")
 os.remove("cache.mp3")
-os.remove("output.mp3")
-os.remove("trying.f0.csv")
+#os.remove("output.mp3")
+# os.remove("trying.f0.csv")
 os.system("clear")
 
 enters(1)
 prints("Task completed. Please check directory for new video.", 0.02)
 for _ in range(5):
     print()
+
+
+
+# import io
+# import os
+# import cv2
+# import moviepy
+# import crepe
+# import numpy
+# import csv
+# import srt
+
+# from time import sleep
+# from google.cloud import translate_v2, speech_v1, texttospeech
+# from google.cloud.speech_v1 import enums
+# from moviepy.editor import *
+
+
+# def sample_long_running_recognize(storage_uri):
+#     """
+#     Transcribe long audio file from Cloud Storage using asynchronous speech
+#     recognition
+
+#     Args:
+#       storage_uri URI for audio file in Cloud Storage, e.g. gs://[BUCKET]/[FILE]
+#     """
+
+#     client = speech_v1.SpeechClient()
+#     audio_channel_count = 2
+#     enable_separate_recognition_per_channel = True
+
+#     # storage_uri = 'gs://cloud-samples-data/speech/brooklyn_bridge.raw'
+
+#     # Sample rate in Hertz of the audio data sent
+#     sample_rate_hertz = 44100
+
+#     # The language of the supplied audio
+#     language_code = "en-US"
+
+#     # Encoding of audio data sent. This sample sets this explicitly.
+#     # This field is optional for FLAC and WAV audio formats.
+#     encoding = enums.RecognitionConfig.AudioEncoding.LINEAR16
+#     config = {
+#         "sample_rate_hertz": sample_rate_hertz,
+#         "language_code": language_code,
+#         "encoding": encoding,
+#         "audio_channel_count": audio_channel_count,
+#         "enable_separate_recognition_per_channel": enable_separate_recognition_per_channel,
+
+#     }
+#     audio = {"uri": storage_uri}
+
+#     operation = client.long_running_recognize(config, audio)
+
+#     print(u"Waiting for operation to complete...")
+#     response = operation.result()
+
+#     trans = []
+
+#     for result in response.results:
+#         # First alternative is the most probable result
+#         alternative = result.alternatives[0]
+#         trans = trans + [alternative.transcript]#print(u"Transcript: {}".format(alternative.transcript))
+    
+#     print(trans)
+
+
+# sample_long_running_recognize("gs://vatt/trying.wav")
